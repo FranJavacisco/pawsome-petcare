42d70a5998919272bdf2c48a4b1b58657fe31e0f
Francisco Lopez <lopezf28@gmail.com>
2024-12-20 20:49:40 -0300

    Fix: Configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/pawsome-petcare/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
   }
-});
+})
