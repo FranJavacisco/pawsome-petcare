43d166f3fc90f6d0bef941f14d98d7b5e0875018
Francisco Lopez <lopezf28@gmail.com>
2024-12-21 23:50:11 -0300

    actualizando vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Configuración para manejar rutas relativas correctamente en desarrollo y producción
 export default defineConfig({
   plugins: [react()],
-  base: '/pawsome-petcare/',
+  base: process.env.NODE_ENV === 'production' ? '/pawsome-petcare/' : '/',
   build: {
-    outDir: 'dist',
-  }
-})
+    outDir: 'dist', // Carpeta de salida para el build
+  },
+});
